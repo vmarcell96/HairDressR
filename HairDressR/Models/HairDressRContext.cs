55d43c2097c9bb31e293b37bfacc8317a3ff4f83
vmarcell96 <vajdam96@gmail.com>
2022-12-30 09:49:38 +0100

    starter project with sqlite

---
+++ HairDressR/Models/HairDressRContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HairDressR.Models
+{
+    public class HairDressRContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=data.db");
+    }
+}
---
+++ HairDressR/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HairDressR.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+    }
+}
